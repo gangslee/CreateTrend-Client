f30e6e74ee4a033d67b270853c87b606922774e4
이경수 <grandnet@naver.com>
2020-08-06 14:08:00 +0900

    Create keyword page's createSlice

---
+++ src/store.tsx
@@ -0,0 +1,67 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+export interface IWordMapData {
+  name: string;
+  children: {
+    name: string;
+    value: number;
+  }[];
+}
+
+export interface ICommentData {
+  avatar: string;
+  comment: string;
+  thumbnail: string;
+}
+
+export interface ILineChartData {
+  name: string;
+  data: {
+    date: string;
+    value: number;
+  }[];
+}
+
+export interface IKeywordChartData {
+  name: string;
+  data: string[];
+}
+
+export interface IVideoListData {
+  name: string;
+  data: {
+    name: string;
+    thumbnail: string;
+    link: string;
+  }[];
+}
+
+export interface IKeywordData {
+  wordmap: IWordMapData[];
+  comment: ICommentData[];
+  lines: ILineChartData[];
+  keyword: IKeywordChartData[];
+  video: IVideoListData[];
+}
+
+const keywordData: IKeywordData = {
+  wordmap: null,
+  comment: null,
+  lines: null,
+  keyword: null,
+  video: null,
+};
+
+const keywordSlice = createSlice({
+  name: "keywordReducer",
+  initialState: keywordData,
+  reducers: {
+    add: (state, action) => {
+      state.wordmap = action.payload.wordmap;
+      state.comment = action.payload.comment;
+      state.lines = action.payload.lines;
+      state.keyword = action.payload.keyword;
+      state.video = action.payload.video;
+    },
+  },
+});
