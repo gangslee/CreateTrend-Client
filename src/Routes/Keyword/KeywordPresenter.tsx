af016a23ba307f034fd51188658fbba25eda6616
이경수 <grandnet@naver.com>
2020-08-06 15:10:27 +0900

    Set Keyword page default layout

--- src/Routes/Keyword/KeywordPresenter.tsx
+++ src/Routes/Keyword/KeywordPresenter.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 1040px;
+  height: 1000px;
+  display: flex;
+  justify-content: space-between;
+  margin: 50px auto;
+`;
+
+const AnalysisSection = styled.div`
+  width: 700px;
+  height: 1000px;
+  /* border: 1px solid #aaa; */
+`;
+
+const VideoSection = styled.div`
+  width: 290px;
+  height: 1000px;
+  border: 1px solid #aaa;
+`;
 
 export default function KeywordPresenter() {
-  return <h1>KeywordPresenter</h1>;
+  return (
+    <Container>
+      <AnalysisSection></AnalysisSection>
+      <VideoSection></VideoSection>
+    </Container>
+  );
 }
