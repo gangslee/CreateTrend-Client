5783e39aae2fd75b3401ba5a31fbdf3155554b57
이경수 <grandnet@naver.com>
2020-08-09 20:57:43 +0900

    Set layout in KeywordPresenter

--- src/Routes/Channel/ChannelPresenter.tsx
+++ src/Routes/Channel/ChannelPresenter.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
 interface IChannelPresenterProps {
   loading: boolean;
 }
 
+const Container = styled.div`
+  width: 1040px;
+  margin: 50px auto;
+`;
+
+const TabContainer = styled.div`
+  height: 75px;
+  margin-bottom: 30px;
+  border: 1px solid #aaa;
+`;
+
+const ResultContainer = styled.div`
+  height: 800px;
+  border: 1px solid #aaa;
+`;
+
 export default function ChannelPresenter({ loading }: IChannelPresenterProps) {
-  return <h1>ChannelPresenter</h1>;
+  return loading ? (
+    <Container>
+      <TabContainer></TabContainer>
+      <ResultContainer></ResultContainer>
+    </Container>
+  ) : (
+    <div>123</div>
+  );
 }
