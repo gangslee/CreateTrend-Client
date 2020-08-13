c0e6062fd3e9e5968888a74353d7a413511b113e
이경수 <grandnet@naver.com>
2020-08-13 13:40:02 +0900

    Set keyword page api

--- src/api.tsx
+++ src/api.tsx
@@ -3,3 +3,12 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://ec2-13-124-107-195.ap-northeast-2.compute.amazonaws.com:8000/',
 });
+
+export const getApi = {
+  keyword: (search: string) =>
+    api.get('keyword_search', {
+      params: {
+        search: search,
+      },
+    }),
+};
