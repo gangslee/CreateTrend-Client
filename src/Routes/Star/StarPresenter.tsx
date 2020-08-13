9621e8815dc0cdba0e0571631b616828cc21cf93
이경수 <grandnet@naver.com>
2020-08-13 22:18:35 +0900

    Set layout in StarPresenter

--- src/Routes/Star/StarPresenter.tsx
+++ src/Routes/Star/StarPresenter.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 1040px;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  margin: 50px auto;
+  border: 1px solid #ddd;
+`;
+
+const AnalysisSection = styled.div`
+  width: 700px;
+  border: 1px solid #ddd;
+  height: 1000px;
+`;
+
+const VideoSection = styled.div`
+  width: 290px;
+  border: 1px solid #ddd;
+  height: 1000px;
+`;
 
 function StarPresenter() {
-  return <h1>StarPresenter</h1>;
+  return (
+    <Container>
+      <AnalysisSection></AnalysisSection>
+      <VideoSection></VideoSection>
+    </Container>
+  );
 }
 
 export default StarPresenter;
