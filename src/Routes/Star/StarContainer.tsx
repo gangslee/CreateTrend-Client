76170a88fae0839081caab15bf77e79416f22982
이경수 <grandnet@naver.com>
2020-08-14 14:29:28 +0900

    Set periodData dispatch in StarContainer

--- src/Routes/Star/StarContainer.tsx
+++ src/Routes/Star/StarContainer.tsx
@@ -5,6 +5,7 @@ import {
   RootState,
   RootDispatch,
   starDataUpdate,
+  periodDataUpdate,
   starPieSliceStateUpdate,
   currentPage,
   IStarState,
@@ -303,18 +304,300 @@ function getData(): IStarState {
   return data;
 }
 
+function getData2(): IStarState {
+  const data: IStarState = {
+    video: [
+      {
+        type: 'analysis',
+        data: [
+          {
+            id: '257R-JkTmcA',
+            name: '시속 1,665km이상 달릴 수 없다면 죽어야하는 상상초월의 대재앙..',
+            thumbnail: 'https://i.ytimg.com/vi/257R-JkTmcA/hqdefault.jpg',
+          },
+          {
+            id: '9VzZ1G8X_I0',
+            name: '상위 0.1% 재벌2세 참교육하는 상위 0.001% 재벌 형사(?)',
+            thumbnail: 'https://i.ytimg.com/vi/9VzZ1G8X_I0/hqdefault.jpg',
+          },
+          {
+            id: 'zS82ef7mQzw',
+            name: '분장을 벗은 영화 캐릭터들의 G리는 실제 모습들..',
+            thumbnail: 'https://i.ytimg.com/vi/zS82ef7mQzw/hqdefault.jpg',
+          },
+          {
+            id: '257R-JkTmcA',
+            name: '시속 1,665km이상 달릴 수 없다면 죽어야하는 상상초월의 대재앙..',
+            thumbnail: 'https://i.ytimg.com/vi/257R-JkTmcA/hqdefault.jpg',
+          },
+          {
+            id: '9VzZ1G8X_I0',
+            name: '상위 0.1% 재벌2세 참교육하는 상위 0.001% 재벌 형사(?)',
+            thumbnail: 'https://i.ytimg.com/vi/9VzZ1G8X_I0/hqdefault.jpg',
+          },
+        ],
+      },
+    ],
+    keyword: {
+      pie: [
+        {
+          name: '지무비',
+          value: 17,
+          wordmap: [
+            {
+              name: '지무비',
+              children: [
+                {
+                  name: '1번 노드',
+                  value: 10,
+                },
+                {
+                  name: '2번 노드',
+                  value: 9,
+                },
+                {
+                  name: '3번 노드',
+                  value: 8,
+                },
+                {
+                  name: '4번 노드',
+                  value: 7,
+                },
+                {
+                  name: '5번 노드',
+                  value: 6,
+                },
+                {
+                  name: '6번 노드',
+                  value: 5,
+                },
+                {
+                  name: '7번 노드',
+                  value: 4,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '채널',
+          value: 16,
+          wordmap: [
+            {
+              name: '채널',
+              children: [
+                {
+                  name: '1번 노드',
+                  value: 10,
+                },
+                {
+                  name: '2번 노드',
+                  value: 9,
+                },
+                {
+                  name: '3번 노드',
+                  value: 8,
+                },
+                {
+                  name: '4번 노드',
+                  value: 7,
+                },
+                {
+                  name: '5번 노드',
+                  value: 6,
+                },
+                {
+                  name: '6번 노드',
+                  value: 5,
+                },
+                {
+                  name: '7번 노드',
+                  value: 4,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '가입',
+          value: 14,
+          wordmap: [
+            {
+              name: '가입',
+              children: [
+                {
+                  name: '1번 노드',
+                  value: 10,
+                },
+                {
+                  name: '2번 노드',
+                  value: 9,
+                },
+                {
+                  name: '3번 노드',
+                  value: 8,
+                },
+                {
+                  name: '4번 노드',
+                  value: 7,
+                },
+                {
+                  name: '5번 노드',
+                  value: 6,
+                },
+                {
+                  name: '6번 노드',
+                  value: 5,
+                },
+                {
+                  name: '7번 노드',
+                  value: 4,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '영화',
+          value: 13,
+          wordmap: [
+            {
+              name: '영화',
+              children: [
+                {
+                  name: '1번 노드',
+                  value: 10,
+                },
+                {
+                  name: '2번 노드',
+                  value: 9,
+                },
+                {
+                  name: '3번 노드',
+                  value: 8,
+                },
+                {
+                  name: '4번 노드',
+                  value: 7,
+                },
+                {
+                  name: '5번 노드',
+                  value: 6,
+                },
+                {
+                  name: '6번 노드',
+                  value: 5,
+                },
+                {
+                  name: '7번 노드',
+                  value: 4,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'naver',
+          value: 13,
+          wordmap: [
+            {
+              name: 'naver',
+              children: [
+                {
+                  name: '1번 노드',
+                  value: 10,
+                },
+                {
+                  name: '2번 노드',
+                  value: 9,
+                },
+                {
+                  name: '3번 노드',
+                  value: 8,
+                },
+                {
+                  name: '4번 노드',
+                  value: 7,
+                },
+                {
+                  name: '5번 노드',
+                  value: 6,
+                },
+                {
+                  name: '6번 노드',
+                  value: 5,
+                },
+                {
+                  name: '7번 노드',
+                  value: 4,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '개인',
+          value: 11,
+          wordmap: [
+            {
+              name: '개인',
+              children: [
+                {
+                  name: '1번 노드',
+                  value: 10,
+                },
+                {
+                  name: '2번 노드',
+                  value: 9,
+                },
+                {
+                  name: '3번 노드',
+                  value: 8,
+                },
+                {
+                  name: '4번 노드',
+                  value: 7,
+                },
+                {
+                  name: '5번 노드',
+                  value: 6,
+                },
+                {
+                  name: '6번 노드',
+                  value: 5,
+                },
+                {
+                  name: '7번 노드',
+                  value: 4,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      current: 0,
+    },
+  };
+  return data;
+}
+
 function mapStateToProps(state: RootState) {
+  console.log(state.star, state.period);
   return {
-    useAble: state.star.useAble,
+    useAble: {
+      star: state.star.useAble,
+      period: state.period.useAble,
+    },
   };
 }
 
 function mapDispatchToProps(dispatch: RootDispatch) {
   return {
-    update: (data: IStarState) => {
-      if (data) {
+    update: (star: IStarState, period: IStarState) => {
+      if (star && period) {
         dispatch(currentPage('star'));
-        dispatch(starDataUpdate(data));
+        dispatch(starDataUpdate(star));
+        dispatch(periodDataUpdate(period));
       }
     },
     stateFuncs: {
@@ -334,10 +617,11 @@ type Props = PropsFromRedux;
 function StarContainer({useAble, update, stateFuncs}: Props) {
   useEffect(() => {
     const sampleData = getData();
-    update(sampleData);
+    const sampleData2 = getData2();
+    update(sampleData, sampleData2);
   }, [update]);
 
-  return <StarPresenter loading={useAble} funcs={stateFuncs} />;
+  return <StarPresenter loading={useAble.star && useAble.period} funcs={stateFuncs} />;
 }
 
 export default connector(StarContainer);
