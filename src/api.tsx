2691372176eba7a7f93a96380a1d71d6bfececf6
이경수 <grandnet@naver.com>
2020-08-27 13:31:04 +0900

    Modify API address

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-13-124-107-195.ap-northeast-2.compute.amazonaws.com:8000/',
+  baseURL: 'http://ec2-13-124-107-195.ap-northeast-2.compute.amazonaws.com/api/',
 });
 
 export const getApi = {
