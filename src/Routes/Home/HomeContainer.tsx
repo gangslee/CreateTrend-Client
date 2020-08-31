85796b79b72da4128b9057a132bd138155d0bbcc
이경수 <grandnet@naver.com>
2020-08-31 13:28:04 +0900

    Set connector in Home Container with Redux store

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useLayoutEffect } from "react";
+import { connect, ConnectedProps } from "react-redux";
 
-import HomePresenter from '../Home/HomePresenter';
+import { RootDispatch, currentPage } from "../../store";
+import HomePresenter from "../Home/HomePresenter";
 
-function HomeContainer() {
+function mapDispatchToProps(dispatch: RootDispatch) {
+  return {
+    setPage: () => dispatch(currentPage("home")),
+  };
+}
+
+const connector = connect(null, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type Props = PropsFromRedux;
+
+function HomeContainer({}: Props) {
+  useLayoutEffect(() => {});
   return <HomePresenter />;
 }
 
-export default HomeContainer;
+export default connector(HomeContainer);
--- src/store.tsx
+++ src/store.tsx
@@ -1,4 +1,4 @@
-import {configureStore, createSlice, combineReducers} from '@reduxjs/toolkit';
+import { configureStore, createSlice, combineReducers } from "@reduxjs/toolkit";
 
 export interface IWordMapData {
   name: string;
@@ -65,7 +65,7 @@ const keywordData: IKeywordData = {
 };
 
 const keywordSlice = createSlice({
-  name: 'keywordReducer',
+  name: "keywordReducer",
   initialState: keywordData,
   reducers: {
     keywordDataUpdate: (state, action) => {
@@ -96,37 +96,44 @@ const statisticsState: IStatisticsState = {
 };
 
 const statisticsSlice = createSlice({
-  name: 'statisticsReducer',
+  name: "statisticsReducer",
   initialState: statisticsState,
   reducers: {
     statisticsDataUpdate: (state, action) => {
       if (action.payload) {
         state.keywordChart = action.payload;
-        state.keywordChart.map((data) => data.keyword.map((word) => (word.visit = false)));
+        state.keywordChart.map((data) =>
+          data.keyword.map((word) => (word.visit = false))
+        );
         state.isChecked = true;
       }
     },
     keywordDetailUpdate: (state, action) => {
       if (action.payload) {
-        state.keywordChart[state.currentChart].keyword[state.currentKeyword].visit = true;
-        state.keywordChart[state.currentChart].keyword[state.currentKeyword].popular = Math.round(
-          action.payload.keyword[0].popular
-        );
-        state.keywordChart[state.currentChart].keyword[state.currentKeyword].wordmap =
-          action.payload.keyword[0].wordmap;
-        state.keywordChart[state.currentChart].keyword[state.currentKeyword].line = [
-          action.payload.keyword[0].lines,
-        ];
-        state.keywordChart[state.currentChart].keyword[state.currentKeyword].video = [
-          action.payload.keyword[0].video,
-        ];
+        state.keywordChart[state.currentChart].keyword[
+          state.currentKeyword
+        ].visit = true;
+        state.keywordChart[state.currentChart].keyword[
+          state.currentKeyword
+        ].popular = Math.round(action.payload.keyword[0].popular);
+        state.keywordChart[state.currentChart].keyword[
+          state.currentKeyword
+        ].wordmap = action.payload.keyword[0].wordmap;
+        state.keywordChart[state.currentChart].keyword[
+          state.currentKeyword
+        ].line = [action.payload.keyword[0].lines];
+        state.keywordChart[state.currentChart].keyword[
+          state.currentKeyword
+        ].video = [action.payload.keyword[0].video];
       }
     },
     disableUseAbleStatistics: (state) => {
       state.isChecked = false;
     },
     chartStateUpdate: (state) => {
-      state.currentChart === 0 ? (state.currentChart = 1) : (state.currentChart = 0);
+      state.currentChart === 0
+        ? (state.currentChart = 1)
+        : (state.currentChart = 0);
       state.currentKeyword = 0;
     },
     keywordStateUpdate: (state, action) => {
@@ -168,7 +175,7 @@ const starState: IStarState = {
 };
 
 const starSlice = createSlice({
-  name: 'starReducer',
+  name: "starReducer",
   initialState: starState,
   reducers: {
     starDataUpdate: (state, action) => {
@@ -195,7 +202,7 @@ const periodState: IStarState = {
 };
 
 const periodSlice = createSlice({
-  name: 'periodReducer',
+  name: "periodReducer",
   initialState: periodState,
   reducers: {
     periodDataUpdate: (state, action) => {
@@ -211,7 +218,7 @@ const periodSlice = createSlice({
 });
 
 const sliderSlice = createSlice({
-  name: 'sliderReducer',
+  name: "sliderReducer",
   initialState: {
     keyword: 0,
     statistics: 0,
@@ -219,29 +226,41 @@ const sliderSlice = createSlice({
   },
   reducers: {
     sliderStateNext: (state, action) => {
-      if (action.payload.page === 'keyword') {
-        state.keyword !== action.payload.len ? (state.keyword += 1) : (state.keyword = 0);
-      } else if (action.payload.page === 'statistics') {
-        state.statistics !== action.payload.len ? (state.statistics += 1) : (state.statistics = 0);
-      } else if (action.payload.page === 'star') {
-        state.star !== action.payload.len ? (state.star += 1) : (state.star = 0);
+      if (action.payload.page === "keyword") {
+        state.keyword !== action.payload.len
+          ? (state.keyword += 1)
+          : (state.keyword = 0);
+      } else if (action.payload.page === "statistics") {
+        state.statistics !== action.payload.len
+          ? (state.statistics += 1)
+          : (state.statistics = 0);
+      } else if (action.payload.page === "star") {
+        state.star !== action.payload.len
+          ? (state.star += 1)
+          : (state.star = 0);
       }
     },
     sliderStatePrev: (state, action) => {
-      if (action.payload.page === 'keyword') {
-        state.keyword !== 0 ? (state.keyword -= 1) : (state.keyword = action.payload.len);
-      } else if (action.payload.page === 'statistics') {
-        state.statistics !== 0 ? (state.statistics -= 1) : (state.statistics = action.payload.len);
-      } else if (action.payload.page === 'star') {
-        state.star !== 0 ? (state.star -= 1) : (state.star = action.payload.len);
+      if (action.payload.page === "keyword") {
+        state.keyword !== 0
+          ? (state.keyword -= 1)
+          : (state.keyword = action.payload.len);
+      } else if (action.payload.page === "statistics") {
+        state.statistics !== 0
+          ? (state.statistics -= 1)
+          : (state.statistics = action.payload.len);
+      } else if (action.payload.page === "star") {
+        state.star !== 0
+          ? (state.star -= 1)
+          : (state.star = action.payload.len);
       }
     },
   },
 });
 
 const pageSlice = createSlice({
-  name: 'pageReducer',
-  initialState: 'home',
+  name: "pageReducer",
+  initialState: "home",
   reducers: {
     currentPage: (state, action) => (state = action.payload),
   },
@@ -260,7 +279,10 @@ const store = configureStore({
   reducer: cReducer,
 });
 
-export const {keywordDataUpdate, disableUseAbleKeyword} = keywordSlice.actions;
+export const {
+  keywordDataUpdate,
+  disableUseAbleKeyword,
+} = keywordSlice.actions;
 
 export const {
   statisticsDataUpdate,
@@ -270,13 +292,13 @@ export const {
   keywordStateUpdate,
 } = statisticsSlice.actions;
 
-export const {starDataUpdate, starPieSliceStateUpdate} = starSlice.actions;
+export const { starDataUpdate, starPieSliceStateUpdate } = starSlice.actions;
 
-export const {periodDataUpdate, periodDateUpdate} = periodSlice.actions;
+export const { periodDataUpdate, periodDateUpdate } = periodSlice.actions;
 
-export const {sliderStateNext, sliderStatePrev} = sliderSlice.actions;
+export const { sliderStateNext, sliderStatePrev } = sliderSlice.actions;
 
-export const {currentPage} = pageSlice.actions;
+export const { currentPage } = pageSlice.actions;
 
 export default store;
 
