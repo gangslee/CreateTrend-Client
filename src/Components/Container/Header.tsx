6f189c4abb2f2e0d933803280be72edabe929d30
이경수 <grandnet@naver.com>
2020-08-31 15:01:02 +0900

    Set Divider in Header

--- src/Components/Container/Header.tsx
+++ src/Components/Container/Header.tsx
@@ -6,10 +6,18 @@ const Container = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: 70px;
-  border: 1px solid #aaa;
+  height: 73px;
+`;
+
+const Divider = styled.div`
+  height: 3px;
+  background-image: linear-gradient(to right, #950707 0%, #fb4242);
 `;
 
 export default function Header() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Divider />
+    </Container>
+  );
 }
