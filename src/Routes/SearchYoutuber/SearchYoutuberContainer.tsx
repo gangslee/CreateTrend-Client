bf76a7b0d6d47307d8d3178ebc58ea8b87a451c2
이경수 <grandnet@naver.com>
2020-09-28 01:55:48 +0900

    Set SearchBar&onSubmit in SearchYoutuber page

--- src/Routes/SearchYoutuber/SearchYoutuberContainer.tsx
+++ src/Routes/SearchYoutuber/SearchYoutuberContainer.tsx
@@ -1,23 +1,47 @@
-import React, { useLayoutEffect } from "react";
-import { RouteComponentProps } from "react-router-dom";
+import React, {useLayoutEffect} from 'react';
+import {RouteComponentProps} from 'react-router-dom';
+import {connect, ConnectedProps} from 'react-redux';
 
-import store from "../../store/store";
-import { fetchData } from "../../actions/searchYoutuber";
-import SearchYoutuberPresenter from "./SearchYoutuberPresenter";
+import store, {RootState} from '../../store/store';
+import {fetchData} from '../../actions/searchYoutuber';
+import SearchYoutuberPresenter from './SearchYoutuberPresenter';
+
+function mapStateToProps(state: RootState) {
+  return {
+    states: {
+      search: state.home,
+    },
+  };
+}
+
+const connector = connect(mapStateToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
 
 interface IParamsProps {
   youtuber_name: string;
 }
 
-function SearchYoutuberContainer(props: RouteComponentProps<IParamsProps>) {
+type Props = PropsFromRedux & RouteComponentProps<IParamsProps>;
+
+function SearchYoutuberContainer({states, match, history}: Props) {
   useLayoutEffect(() => {
-    fetchData(
-      store.getState(),
-      store.dispatch,
-      props.match.params.youtuber_name
-    );
-  });
-  return <SearchYoutuberPresenter />;
+    fetchData(store.getState(), store.dispatch, match.params.youtuber_name);
+  }, [match]);
+
+  const searchKeyword = async () => {
+    if (match.params.youtuber_name === states.search.searchTerm && states.search.searchType === 1) {
+      fetchData(store.getState(), store.dispatch, match.params.youtuber_name);
+    } else {
+      history.push(
+        `/${states.search.searchType === 0 ? 'keyword' : 'searchYoutuber'}/${
+          states.search.searchTerm
+        }`
+      );
+    }
+  };
+
+  return <SearchYoutuberPresenter searchKeyword={searchKeyword} />;
 }
 
-export default SearchYoutuberContainer;
+export default connector(SearchYoutuberContainer);
--- src/Routes/SearchYoutuber/SearchYoutuberPresenter.tsx
+++ src/Routes/SearchYoutuber/SearchYoutuberPresenter.tsx
@@ -1,7 +1,26 @@
-import React from "react";
-import { connect, ConnectedProps } from "react-redux";
+import React from 'react';
+import {connect, ConnectedProps} from 'react-redux';
+import styled, {css} from 'styled-components';
 
-import { RootState } from "../../store/store";
+import {RootState} from '../../store/store';
+import {BGSecond} from '../../Components/Container/BGContiner';
+import SearchBar from '../../Components/Container/SearchBar';
+
+const Slogan = styled.div`
+  font-family: 'S-CoreDream-5Medium';
+  font-size: 30px;
+  text-align: center;
+  margin: 70px 0px;
+`;
+
+const SloganRed = styled.span`
+  color: #dd0909;
+`;
+
+const SearchBarContainer = styled.form`
+  display: flex;
+  justify-content: center;
+`;
 
 function mapStateToProps(state: RootState) {
   return {
@@ -17,18 +36,25 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type Props = PropsFromRedux;
 
-function SearchYoutuberPresenter({ states }: Props) {
-  console.log(states);
+interface ISearchYoutuberProps extends Props {
+  searchKeyword: () => void;
+}
+
+function SearchYoutuberPresenter({states, searchKeyword}: ISearchYoutuberProps) {
+  const handleOnSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    searchKeyword();
+  };
+
   return (
-    <>
-      <h1>SearchYoutuberPresenter</h1>
-      <h1>SearchYoutuberPresenter</h1>
-      <h1>SearchYoutuberPresenter</h1>
-      <h1>SearchYoutuberPresenter</h1>
-      <h1>SearchYoutuberPresenter</h1>
-      <h1>SearchYoutuberPresenter</h1>
-      <h1>SearchYoutuberPresenter</h1>
-    </>
+    <BGSecond>
+      <Slogan>
+        "궁금한 <SloganRed>스타채널</SloganRed>을 검색해 보세요"
+      </Slogan>
+      <SearchBarContainer onSubmit={handleOnSubmit}>
+        <SearchBar searchKeyword={searchKeyword} />
+      </SearchBarContainer>
+    </BGSecond>
   );
 }
 
