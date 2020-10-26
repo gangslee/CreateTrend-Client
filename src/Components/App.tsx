0b5cbe633f5fe00fcad17a69e24a14be33dc6de2
이경수 <grandnet@naver.com>
2020-10-26 14:07:43 +0900

    Set pageview pixel in app

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,13 +1,21 @@
 import React, {useLayoutEffect} from 'react';
+import ReactPixel from 'react-facebook-pixel';
+
 import Router from './Router';
 import GlobalStyles from './GlobalStyles';
-
 import store from '../store/store';
 import {loadUser} from '../actions/auth';
+
+const options = {
+  autoConfig: true, // set pixel's autoConfig
+  debug: false, // enable logs
+};
 
 function App() {
   useLayoutEffect(() => {
     loadUser(store.getState(), store.dispatch);
+    ReactPixel.init('2474669376160852', null, options);
+    ReactPixel.track('PageView', null);
   });
   return (
     <>
